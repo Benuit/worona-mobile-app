18667801a2ff401b86ad448c1aa298c6e7e3a51d
LuisHerranz <luisherranz@gmail.com>
2014-06-10 12:09:12 +0200

    removing zero brane in the boilerplate, adding worona.wp_url

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
--- ZERO BRANE STUDIO DEBUGGER
---require("mobdebug").start()
-
 -- WORONA INITIALIZATION
 local worona = require "worona"
 worona:init()
 
 -- START THE LOG
 worona.log:start( { level = "INFO", reset_log = true, console = true, file = "log", previous_file = "previous_log" } )
+
+-- WORDPRESS URL
+worona.wp_url = "http://www.worona.org"
 
 -- START THE APPLICATION
 worona:do_action( "init" )
