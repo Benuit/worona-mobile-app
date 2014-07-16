8fb18b4ee065138e3ea134572643fe8b92d82540
LuisHerranz <luisherranz@gmail.com>
2014-07-16 13:13:58 +0200

    change to example.org to push it to the upstream

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ worona:initializeWoronaExtensions()
 worona.log:start( { level = "INFO", reset_log = true, console = true, file = "log", previous_file = "previous_log" } )
 
 -- WORONA CONFIGURATION
-worona.wp_url        = "http://www.govoid.dev"  --. Enter the url from which the app will read the data.
+worona.wp_url        = "http://www.example.org"  --. Enter the url from which the app will read the data.
 worona.current_style = "flat-ui"
 
 -- START THE APPLICATION
