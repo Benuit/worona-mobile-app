2eb5e20cc18de327a2331ffbe58d3506d1a8d5a2
LuisHerranz <luisherranz@gmail.com>
2014-07-16 13:11:40 +0200

    change app_url for wp_url (it's more clear) and add current_style to choose the style

--- main.lua
+++ main.lua
@@ -5,12 +5,9 @@ worona:initializeWoronaExtensions()
 -- START THE LOG
 worona.log:start( { level = "INFO", reset_log = true, console = true, file = "log", previous_file = "previous_log" } )
 
-
 -- WORONA CONFIGURATION
-worona.app_url = "http://www.govoid.dev"  --. Enter the url from which the app will read the data.
-
--- WORDPRESS URL
-worona.wp_url = "http://www.worona.org"
+worona.wp_url        = "http://www.govoid.dev"  --. Enter the url from which the app will read the data.
+worona.current_style = "flat-ui"
 
 -- START THE APPLICATION
 worona:do_action( "load_style" )
