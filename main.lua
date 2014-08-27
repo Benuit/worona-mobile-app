64f4bf5fd9c2649d1d522e2ca63ed72aa86912c6
LuisHerranz <luisherranz@gmail.com>
2014-08-27 14:09:57 +0200

    commit so I can go back to initial state

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+-- ZERO BRANE STUDIO DEBUGGE
+-- require("mobdebug").start()
+
 -- WORONA INITIALIZATION
 local worona = require "worona"
 worona:initializeWoronaExtensions()
@@ -6,9 +9,14 @@ worona:initializeWoronaExtensions()
 worona.log:start( { level = "INFO", reset_log = true, console = true, file = "log", previous_file = "previous_log" } )
 
 -- WORONA CONFIGURATION
-worona.wp_url        = "http://www.example.org"  --. Enter the url from which the app will read the data.
-worona.current_style = "flat-ui"
+worona.current_style         = "flat-ui"
+worona.wp_url                = "http://www.worona.org"  --. Enter the url from which the app will read the data.
+worona.app_title             = "Worona Blog"
+worona.app_about_title       = "About Worona"
+worona.app_about_description = "This is the worona.org blog."
 
 -- START THE APPLICATION
 worona:do_action( "load_style" )
 worona:do_action( "init" )
+worona:do_action( "go_to_scene", { scene_type = "scene-list", effect = "fade", time = 500 } )
+-- worona:do_action( "load_url", { url = "http://www.civitatis.com" } )
