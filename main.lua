c5bf46a0f3e1eb01e870c9742d6b58a91a1a319f
LuisHerranz <luisherranz@gmail.com>
2014-10-16 16:54:07 +0200

    add stats variable, config comments and update worona-app submodule

--- main.lua
+++ main.lua
@@ -9,11 +9,12 @@ worona:initializeWoronaExtensions()
 worona.log:start( { level = "WARNING", reset_log = true, console = true, file = "log", previous_file = "previous_log" } )
 
 -- WORONA CONFIGURATION
-worona.current_style         = "flat-ui"
-worona.wp_url                = "http://www.worona.org"  --. Enter the url from which the app will read the data.
-worona.app_title             = "Worona Blog"
-worona.app_about_title       = "About Worona"
+worona.current_style         = "flat-ui"				-- Choose your theme.
+worona.wp_url                = "http://www.worona.org"  -- Enter the url from which the app will read the data.
+worona.app_title             = "Worona Blog"			-- Enter the main title of your app.
+worona.app_about_title       = "About Worona"			-- Enter the title of the about section.
 worona.app_about_description = "Worona is a Wordpress plugin that enables you to create native apps without programming knowledge. Once installed you can use WordPress to configure your app and create content.\n\nWorona is an open source initiative to make Wordpress native in the mobile world. And just like Wordpress, you can extend it with extensions made by you or the community.\n\nYou can visit us and learn more at www.worona.org."
+worona.stats				 = true 					-- Stats about worona usage.
 
 -- START THE APPLICATION
 worona:do_action( "load_style" )
