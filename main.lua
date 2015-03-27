d39564319fb015a939870dc93a4b9fcdd6361a77
Fernando Moreno <fmorenoper@gmail.com>
2015-03-27 18:04:46 +0100

    Getting ready for 0.7

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 -- ZERO BRANE STUDIO DEBUGGE
 -- require("mobdebug").start()
 
+-- WORONA MOBILE APP VERSION: 0.7
+
 -- WORONA INITIALIZATION
 local worona = require "worona"
 require "worona-config.about"
@@ -14,6 +16,7 @@ worona.current_style       = "default"				-- Choose your theme.
 worona.wp_url              = "http://www.worona.org"  -- Enter the url from which the app will read the data.
 worona.app_title           = "Worona Blog"			-- Enter the main title of your app.
 worona.app_number_of_posts = 20			-- Choose the max number of posts to be shown in your app. Worona will always retrieve the latest posts.
+worona.content_type        = "post"
 
 -- ABOUT PAGE
 -- Configure About page options in: worona-mobile-app/worona-config/about.lua
@@ -25,4 +28,4 @@ worona.stats = true  -- Stats about worona usage.
 -- START THE APPLICATION
 worona:do_action( "load_style" )
 worona:do_action( "init" )
-worona:do_action( "go_to_scene", { scene_type = "scene-list", effect = "fade", time = 500 } )
+worona:do_action( "go_to_scene", { scene_type = "scene-list", effect = "fade", time = 500, params = { show_posts = "all" } } )
