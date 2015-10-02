148e4e9d94da103c16e4dd1c007ae90bbc70cb87
Luis Herranz <luisherranz@gmail.com>
2015-10-02 18:18:58 +0200

    bump version

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- ZERO BRANE STUDIO DEBUGGE
 -- require("mobdebug").start()
 
--- WORONA MOBILE APP VERSION: 0.7.1
+-- WORONA MOBILE APP VERSION: 0.7.2
 
 -- WORONA INITIALIZATION
 local worona = require "worona"
