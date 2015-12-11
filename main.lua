b0e95b527363308f3febf8f171464424b170f86b
Fernando Moreno <fer@worona.org>
2015-12-11 12:55:05 +0100

    Updated version

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- ZERO BRANE STUDIO DEBUGGE
 -- require("mobdebug").start()
 
--- WORONA MOBILE APP VERSION: 0.7.4
+-- WORONA MOBILE APP VERSION: 0.7.5
 
 -- WORONA INITIALIZATION
 local worona = require "worona"
